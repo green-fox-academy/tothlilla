0007c318c89a9cec2100a3b4642c79ea4f3445a9
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-01-18 15:31:14 +0100

    We used single for loop for repeating a sentence 100 times

---
+++ week-01/day-3/Ex_23_WontCheat/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // Create a program that writes this line 100 times:
+    for (int i = 0; i < 100; i++) {
+        std::cout << "I won't cheat on the exam!" << std::endl;
+    }
+    // "I won't cheat on the exam!"
+
+    return 0;
+}
