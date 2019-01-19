d80004d9b0972ef200008752ed87f38e91c681f3
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-01-19 14:41:12 +0100

    We append an a to each elements of an array

---
+++ week-01/day-4/Ex_16_AppendA/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `animals`
+    //   with the following content: `["koal", "pand", "zebr"]`
+    // - Add all elements an `"a"` at the end
+
+    std::string animals[] = {"koal", "pand", "zebr"};
+
+    for (int i = 0; i < 3; i++){
+        animals[i] = animals[i] + "a";
+        std::cout << animals[i] << " ";
+    }
+
+
+    return 0;
+}
