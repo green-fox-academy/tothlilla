76f8f82b971b5f121127bb2071ed0bcf47470660
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-01-20 13:14:23 +0100

    I tried to make it with int function (int(&array)[])

--- week-01/day-4/Ex_14_DoubleItems/main.cpp
+++ week-01/day-4/Ex_14_DoubleItems/main.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 #include <string>
 
-int doublearray (int&);
+int doublearray (int(&examplearray)[]);
 
 int main(int argc, char* args[]) {
 
     // - Create an array variable named `numList`
     //   with the following content: `[3, 4, 5, 6, 7]`
     int numList [] = {3, 4, 5, 6, 7};
+    int size = sizeof(numList) / sizeof(numList[0]);
     // - Double all the values in the array
+
     std::cout << doublearray(numList) <<std::endl;
 
     return 0;
 
 }
 
-int  doublearray (int& examplearray){
-    int size = sizeof(examplearray)/sizeof(examplearray [0]);
+int  doublearray (int(&examplearray)[]){
+    int size = sizeof(&examplearray) / sizeof(examplearray[0]);
+    int newarray[size];
     for (int i = 0; i < size; ++i) {
-        int newarray[size];
-        int middle;
-        middle = 2 * examplearray[i];
-        newarray[i]= middle;
-        return newarray[size];
+        newarray[i]= 2 * examplearray[i];
     }
-
+    return newarray[size];
 
 }
 /*
