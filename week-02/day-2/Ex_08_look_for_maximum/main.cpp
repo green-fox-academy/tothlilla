7530b3c43f251e4c75376c8422a362f2b9d9e2a8
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-01-22 17:28:28 +0100

    We compared numbers to find the maximum one in a function

---
+++ week-02/day-2/Ex_08_look_for_maximum/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int maximumnumb (const int examplearray[], int size){
+
+    int miximaxi=examplearray[0];
+
+    for (int i = 0; i < size; ++i) {
+
+        if (miximaxi < examplearray[i]){
+            miximaxi = examplearray [i];
+        }
+
+    }
+    return miximaxi;
+}
+
+int main()
+{
+    int howMany;
+    std::cout << "How many numbers want to compare?" << std::endl; // Create a program which first asks for a number
+    std::cin >> howMany;     // this number indicates how many integers we want to store in an array
+    std::cout << "Write your numbers :" << std::endl;
+
+    int myArray[howMany];
+    for (int i = 0; i < howMany; ++i) {
+        std::cin >> myArray[i];     // and than asks for numbers till the user fills the array
+    }
+    int maxi = maximumnumb(myArray, howMany);
+
+    std::cout << maxi << std::endl;
+    // It should print out the biggest number in the given array and the memory address of it
+
+    return 0;
+}
