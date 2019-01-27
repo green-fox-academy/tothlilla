974c412125eff3e4cb12a4a7cee26d89042422e4
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-01-27 09:13:02 +0100

    We replace and insert certain parts of a string

---
+++ week-02/day-1/Ex_02_urlfixer/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *args[]) {
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crucial component, find out what it is and insert it too!
+
+    std::string url("https//www.reddit.com/r/nevertellmethebots"); //The original string
+    std::string need_expression = "bots"; //The part of the the string we want to change
+
+    int start_replace = url.find(
+            need_expression); //Determinding the beginning index of the changing word with x.find function
+    url.replace(start_replace, need_expression.length(), "odds"); //Replacing original part with expression "odds"
+    //First is the index number, next is the length of changing part, finished by new expression
+
+
+    std::string need_char = ":"; //The string of new part
+    url.insert(5, need_char, 0, 1); //The method of inserting new parts in a string.
+    //First the beginning index, second the new part (as a string),
+    // third is the beginning index of new part you need to insert, the last is the closing index of new part you need to insert
+
+
+    std::cout << url << std::endl; //Printing of the new string
+
+    return 0;
+}
