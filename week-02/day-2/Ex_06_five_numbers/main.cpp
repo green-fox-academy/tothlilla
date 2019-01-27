1f277f86b0db03c67b1967cce9b6a32bab266acf
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-01-27 19:55:39 +0100

    We input 5 elements in an array. We made a pointer of the array and give back the original numbers by dereference

---
+++ week-02/day-2/Ex_06_five_numbers/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+
+
+    int a;
+    int mynumbers[5];
+    std::cout << "Write 5 numbers: " <<std::endl;
+    for (int i = 0; i < 5; i++) { // Create a program which accepts five integers from the console (given by the user)
+        std::cin >> a;            // and store them in an array
+        mynumbers[i] = a;
+    }
+
+    int *mynumbersPtr = mynumbers;
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << *mynumbersPtr <<std::endl; // print out the values of that array using pointers again
+        ++mynumbersPtr;
+    }
+/*
+    for (int i = 0; i < 5; i++) {
+        std::cout << *(mynumbersPtr + i) <<std::endl; // print out the values of that array using pointers again
+    }
+    */
+    return 0;
+}
