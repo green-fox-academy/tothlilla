4d93903ab27afc41a895b5e00436da1ea0189890
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-01-27 10:47:00 +0100

    I made commits to my solution

--- week-02/day-1/Ex_08_appendaletter/main.cpp
+++ week-02/day-1/Ex_08_appendaletter/main.cpp
@@ -2,20 +2,18 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> appendA(std::vector<std::string> savanna){
-    std::vector<std::string> changedAnimals;
-    for(unsigned int i =0; i <savanna.size(); i++){
-        changedAnimals.push_back(savanna[i]);
-        changedAnimals[i] = changedAnimals[i]+"a";
+std::vector<std::string> appendA(std::vector<std::string> savanna)
+{
+    std::vector<std::string> changedAnimals; //We create a vector typed string to fill with the elements of the original vector
+    for (unsigned int i = 0; i < savanna.size(); i++) {
+        changedAnimals.push_back(savanna[i]); //We fill new vector with x.push_back command
+        changedAnimals[i] = changedAnimals[i] + "a"; //And give an "a"letter to each element
     }
-
-
 
     return changedAnimals;
 }
 
-int main(int argc, char* args[])
-{
+int main(int argc, char *args[]) {
     std::vector<std::string> animals = {"bo", "anacond", "koal", "pand", "zebr"};
 
     // Create a function called "appendA()" that adds "a" to every string in the "animals" list.
@@ -23,7 +21,7 @@ int main(int argc, char* args[])
 
     std::vector<std::string> newAnimals = appendA(animals);
 
-    for(int i = 0; i < newAnimals.size(); ++i) {
+    for (int i = 0; i < newAnimals.size(); ++i) {
         std::cout << newAnimals[i] << " ";
     }
 
