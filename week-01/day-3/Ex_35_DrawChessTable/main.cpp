70e0e2eac045f030be2e1c244446b499ae892ca4
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-01 23:02:49 +0100

    We use for loop, if-else condition and two while condition nested into eachother

--- week-01/day-3/Ex_35_DrawChessTable/main.cpp
+++ week-01/day-3/Ex_35_DrawChessTable/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char* args[])
+{
     // Crate a program that draws a chess table like this
     //
     // % % % %
@@ -16,21 +16,19 @@ int main(int argc, char* args[]) {
     int chessRow = 8;
     for (int i = 0; i < chessRow; ++i) {
         for (int j = 0; j < chessRow; ++j) {
-            if (i % 2 == 1) {
+            if (i % 2 == 1) {         //the case of odd rows
                 while (j < chessRow / 2) {
                     std::cout << " %";
                     ++j;
                 }
             } else {
-                while (j < chessRow / 2) {
+                while (j < chessRow / 2) { // //the case of even rows
                     std::cout << "% ";
                     ++j;
                 }
             }
-
         }
         std::cout << std::endl;
-
     }
     return 0;
 }
