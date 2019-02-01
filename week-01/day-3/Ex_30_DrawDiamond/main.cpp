a72c4d8b371cb44c2ee0c736742bc340983e2913
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-01 21:52:30 +0100

    I finally done the diamond

--- week-01/day-3/Ex_30_DrawDiamond/main.cpp
+++ week-01/day-3/Ex_30_DrawDiamond/main.cpp
@@ -19,20 +19,27 @@ int main(int argc, char* args[]) {
     int startSpace = diamondLine/2;
     int startStar = 1;
 
-    for (int k = 0; k < diamondLine/2; ++k) {
+    for (int i = 0; i < diamondLine/2; ++i) {
         for (int j = 0; j < startSpace; ++j) {
-            std::cout << " ";
-
+          std::cout << " ";
         }
-        for (int j = 0; j < startStar; ++j) {
-            std::cout << "*";
+        for (int k = 0; k < startStar; ++k) {
+          std::cout << "*";
         }
         std::cout << "\n";
         startSpace--;
-        startStar = startStar+2;
+        startStar = startStar + 2;
     }
-
-
-    return 0;
-
+  for (int l = 0; l < diamondLine / 2 +1 ; ++l) {
+      for (int j = 0; j < startSpace; ++j) {
+        std::cout << " ";
+      }
+      for (int m = 0; m < startStar; ++m) {
+        std::cout << "*";
+      }
+      std::cout << "\n";
+      startSpace++;
+      startStar = startStar - 2;
+  }
+      return 0;
 }
