c7908a5778e0ef7b84ef73f7661c10391e71f562
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-02 11:59:34 +0100

    I dissmissed myself with this task again. The solution is there without function

--- week-01/day-4/Ex_14_DoubleItems/main.cpp
+++ week-01/day-4/Ex_14_DoubleItems/main.cpp
@@ -1,46 +1,20 @@
 #include <iostream>
 #include <string>
 
-int doublearray (int(&examplearray)[]);
-
 int main(int argc, char* args[]) {
+  // - Create an array variable named `numList`
+  //   with the following content: `[3, 4, 5, 6, 7]`
+  int numList [] = {3, 4, 5, 6, 7};
+  int size = sizeof(numList) / sizeof(numList[0]);
+  // - Double all the values in the array
 
-    // - Create an array variable named `numList`
-    //   with the following content: `[3, 4, 5, 6, 7]`
-    int numList [] = {3, 4, 5, 6, 7};
-    int size = sizeof(numList) / sizeof(numList[0]);
-    // - Double all the values in the array
+      for (int i = 0; i < size; ++i) {
+          numList[i] = numList[i] * 2;
+      }
+      for (int j = 0; j < size; ++j) {
+          std::cout << numList[j] << " ";
+      }
+      std::cout << std::endl;
 
-    std::cout << doublearray(numList) <<std::endl;
-
-    return 0;
-
+      return 0;
 }
-
-int  doublearray (int(&examplearray)[]){
-    int size = sizeof(&examplearray) / sizeof(examplearray[0]);
-    int newarray[size];
-    for (int i = 0; i < size; ++i) {
-        newarray[i]= 2 * examplearray[i];
-    }
-    return newarray[size];
-
-}
-/*
-#include <iostream>
-#include <string>
-
-int main(int argc, char* args[]) {
-
-    int numList[] ={3, 4, 5, 6, 7};
-    for (int i = 0; i < sizeof(numList)/sizeof(numList[0]); ++i) {
-        numList[i] = numList[i] * 2;
-    }
-    for (int j = 0; j < sizeof(numList)/sizeof(numList[0]); ++j) {
-        std::cout << numList[j] << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
-}
- */
