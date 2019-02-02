6f868c70bd0f10934c50d64b1b16d0e18194bc0a
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-02 12:52:23 +0100

    Bad solutiongit add .!

--- week-01/day-4/Ex_20_Unique/main.cpp
+++ week-01/day-4/Ex_20_Unique/main.cpp
@@ -1,74 +1,33 @@
 #include <iostream>
 #include <string>
+#include <bits/valarray_before.h>
 
-int main(int argc, char* args[]) {
+int unique(int numbers[], int size);
 
-    //  Create a function that takes a list of numbers as a parameter
-    //  Don't forget you have pass the size of the list as a parameter as well
-    //  Returns a list of numbers where every number in the list occurs only once
+int main(int argc, char* args[])
+{
+  //  Create a function that takes a list of numbers as a parameter
+  //  Don't forget you have pass the size of the list as a parameter as well
+  //  Returns a list of numbers where every number in the list occurs only once
+  //  Example
+  int numbers[] = {1, 11, 34, 11, 52, 61, 1, 34};
+  int size = sizeof(numbers) / sizeof(numbers[0]);
 
-    //  Example
+  std::cout << (unique(numbers, size)) << std::endl;
+  //  should print: `[1, 11, 34, 52, 61]`
+  return 0;
+}
 
-    int i;
-    int numbers[] = {1, 11, 34, 11, 52, 61, 1, 34};
-    int size = sizeof(numbers)/ sizeof(numbers[0]);
-    int unique[size][size];
+int unique(int numbers[], int size)
+{
+    int prev = 1;
+    int uniqueCounter[prev - 1];
     for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            unique[i] [j]= n#include <iostream>
-
-int main(int argc, char *args[]) {
-
-    int a;
-    std::cout << "Please give me a number: " << std::endl;
-    std::cin >> a;
-    std::string star = "*";
-    std::string space = " ";
-
-
-        for (int i = 0; i < a; i++) {
-
-            for (int x = i; x < a; x++) {
-
-                std::cout << space;
-            }
-            for (int j = 0; j <= i * 2; j++) {
-                std::cout << star;
-
-            }
-            std::cout << std::endl;
-
-        }
-
-
-
-
-
-    // Write a program that reads a number from the standard input, then draws a
-    // pyramid like this:
-    //
-    //
-    //    *
-    //   ***
-    //  *****
-    // *******
-    //
-    // The pyramid should have as many lines as the number was
-
-    return 0;
-}umbers[i];
+        if (numbers[i] != numbers[prev]){
+            uniqueCounter[i] = numbers[i];
+            prev++;
         }
     }
 
-    std::cout << "Give size of the list: " << std::endl;
-    std::cin >> numbers [i];
-
-
-    int middle = numbers [i];
-    if (numbers [i] {
-        std::cout << (unique(numbers)) << std::endl;
-    }
-    //  should print: `[1, 11, 34, 52, 61]`
-
-    return 0;
+    return uniqueCounter[prev];
 }
