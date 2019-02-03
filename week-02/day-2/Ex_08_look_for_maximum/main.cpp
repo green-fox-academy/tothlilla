086dae16adb4a1c6c1701a1ea4a6c09fc5f2ab85
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-03 08:52:42 +0100

    One more solution giving back memory address

--- week-02/day-2/Ex_08_look_for_maximum/main.cpp
+++ week-02/day-2/Ex_08_look_for_maximum/main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
-int maximumnumb (const int examplearray[], int size){
-
+int maximumnumb (const int examplearray[], int size)
+{
     int miximaxi=examplearray[0];
-
     for (int i = 0; i < size; ++i) {
-
         if (miximaxi < examplearray[i]){
             miximaxi = examplearray [i];
         }
-
     }
     return miximaxi;
 }
@@ -20,7 +17,6 @@ int main()
     std::cout << "How many numbers want to compare?" << std::endl; // Create a program which first asks for a number
     std::cin >> howMany;     // this number indicates how many integers we want to store in an array
     std::cout << "Write your numbers :" << std::endl;
-
     int myArray[howMany];
     for (int i = 0; i < howMany; ++i) {
         std::cin >> myArray[i];     // and than asks for numbers till the user fills the array
@@ -28,7 +24,7 @@ int main()
     int maxi = maximumnumb(myArray, howMany);
     int *prprprpr = &maxi;
 
-    std::cout << maxi << "\n" <<prprprpr << std::endl;
+    std::cout << maxi << "\n" << prprprpr << "\n" << &maxi << std::endl;
     // It should print out the biggest number in the given array and the memory address of it
 
     return 0;
