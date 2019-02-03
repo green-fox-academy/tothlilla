a042449c85ab0f42102c68684b6d17ce8724c4b3
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-03 08:38:27 +0100

    Giving a new solution to adding numbers with pointers

--- week-02/day-2/Ex_04_adding/main.cpp
+++ week-02/day-2/Ex_04_adding/main.cpp
@@ -3,16 +3,18 @@
 int main()
 {
     // Add two numbers using pointers
-
+    //First solution
     int a = 20;
     int b = 17;
-
     int c = a + b;
     int *p_c = &c;
-
-
-
     std::cout << *p_c << "\n" << c << "\n" << &c << std::endl;
-
+    //Second solution
+    int d = 20;
+    int e = 17;
+    int *p_d = &d;
+    int *p_e = &e;
+    int sum = *p_e + *p_d;
+    std::cout << *p_d << "\n" << *p_e << "\n" << sum << std::endl;
     return 0;
 }
