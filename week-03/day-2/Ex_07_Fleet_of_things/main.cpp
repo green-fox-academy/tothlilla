d7bf886f8cb39277d7eb1fe537b9bb746591a549
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-03 14:38:06 +0100

    Change the fourth object the right one

--- week-03/day-2/Ex_07_Fleet_of_things/main.cpp
+++ week-03/day-2/Ex_07_Fleet_of_things/main.cpp
@@ -14,32 +14,27 @@
 
 int main(int argc, char* args[])
 {
-
     // Create a fleet of things to have this output:
     // 1. [ ] Get milk
     // 2. [ ] Remove the obstacles
     // 3. [x] Stand up
     // 4. [x] Eat lunch
-    Fleet fleet;
 
+    Fleet fleet;
 
     Thing first("Get milk"); //first is the First object
     fleet.add(first); //we add the first object to the result, and changed the value of result
 
-
     Thing second("Remove the obstacles"); //second is the Second object
     fleet.add(second); //we add the second object to the result, and changed the value of result
-
 
     Thing third("Stand up"); //third is the Third object
     third.complete(); //we manipulate the third object with a bool
     fleet.add(third); //we add the third object to the result, and changed the value of result
 
-
-
     Thing fourth("Eat lunch"); //fourth is the Fourth object
-    third.complete(); //we manipulate the fourth object with a bool
-    fleet.add(third); //we add the fourth object to the result, and changed the value of result
+    fourth.complete(); //we manipulate the fourth object with a bool
+    fleet.add(fourth); //we add the fourth object to the result, and changed the value of result
 
 
     std::cout << fleet.toString() << std::endl;
