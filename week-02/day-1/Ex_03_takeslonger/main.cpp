ecb05d1c415550794f56ebfa82b390bfbce0fc18
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-11 19:14:23 +0100

    using variable instead of values

--- week-02/day-1/Ex_03_takeslonger/main.cpp
+++ week-02/day-1/Ex_03_takeslonger/main.cpp
@@ -8,14 +8,17 @@ int main(int argc, char *args[]) {
     std::string quote("Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.");
     //The original string
     std::string quote_need = "always takes longer than "; //The sting we want to insert
-    std::string quote_to_it = "Hofstadter's Law: It ";
+    //std::string quote_to_it = "Hofstadter's Law: It ";
 
 
-    quote.replace(quote_to_it.length(), 0, quote_need);
+    //quote.replace(quote_to_it.length(), 0, quote_need);
     //First is the index number - but not with x.find function because of spaces!!!
     // Next is the length of changing part, which is zero because we want to insert something, not delete it
     // Finished by new expression
 
+    std::string finding_expr = "you";
+    int index_pos = quote.find(finding_expr);
+    quote.replace(index_pos, 0, quote_need); //because the length is zero, we effectively inset the string
 
     std::cout << quote << std::endl; //Printing of changed string
     return 0;
