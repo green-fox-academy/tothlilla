ad223a07b0b7fd8e489d640364f6b28f284117b6
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-25 12:02:20 +0100

    Greeting in 3 different lines

---
+++ week-06/day-1/Ex_02_helloOthers/main.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    char nameOne[] = "Gergo Kovacs";
+    char nameTwo[] = "Boro Szabo";
+    char nameThree[] = "Tamas Nemeth";
+
+    printf("Hello, %s!\n", nameOne);
+    printf("Hello, %s!\n", nameTwo);
+    printf("Hello, %s!\n", nameThree);
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    return 0;
+}
