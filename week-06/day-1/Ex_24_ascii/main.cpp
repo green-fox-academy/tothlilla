34b8a5340b578abc4c2cdef550c9a8f2c525f6e3
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-25 21:53:24 +0100

    print green fox

---
+++ week-06/day-1/Ex_24_ascii/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "string.h"
+
+// print out the characters that corresponds to these ascii values
+
+int main()
+{
+    int array[] = {103, 114, 101, 101, 110, 32, 102, 111, 120};
+    int size = sizeof(array)/ sizeof(array[0]);
+    for (int i = 0; i < size; ++i) {
+        printf("%c", array[i]);
+    }
+  return 0;
+}
