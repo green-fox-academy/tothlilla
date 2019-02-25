aacad3ee5af1f6820621e0dafec4973c2db6c1e1
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-25 13:51:07 +0100

    just for fun

---
+++ week-06/day-1/Ex_08_organizing_2/main.cpp
@@ -0,0 +1,20 @@
+//Continue working on the pi.c project
+//Organise the function from the previous excercise to separate .c and .h files
+//Create another function which calculates the circumference of a circle
+//the radius of the circle should be passed as a parameter
+//the function should return the calculated circumference
+//circumference = 2 * radius * PI
+//this function should be in the same .c and .h files as the one which calculates the area
+
+#include <stdio.h>
+#include "pi.h"
+
+int main()
+{
+    float radius;
+    printf("Give me the radius: \n");
+    scanf("%f", &radius);
+    printf("area = %.2f\n", AREA(radius));
+    printf("circumference = %.2f", CIRCUMFERENCD(radius));
+    return 0;
+}
---
+++ week-06/day-1/Ex_08_organizing_2/pi.h
@@ -0,0 +1,12 @@
+//
+// Created by Lilla on 2019. 02. 18..
+//
+
+#ifndef EX_08_ORGANIZING_2_PI_H
+#define EX_08_ORGANIZING_2_PI_H
+#define PI 3.14
+#define AREA(radius) (radius * radius * PI)
+#define CIRCUMFERENCD(radius) (2 * radius * PI)
+
+
+#endif //EX_08_ORGANIZING_2_PI_H
