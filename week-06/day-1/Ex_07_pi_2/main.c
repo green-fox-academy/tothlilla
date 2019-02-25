dc23c5fbfb033086e5e34ce8e0944c818f6bb545
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-25 13:14:59 +0100

    defining a variable and a function

---
+++ week-06/day-1/Ex_07_pi_2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+#define AREA(radius) (radius * radius * PI)
+
+// define a variable called PI with the value of 3.14
+// create a function which takes the radius of a circle as a parameter
+// and return the area of that cirle
+// area = radius * radius * PI
+
+int main()
+{
+    float area, radius;
+    printf("Give me the radius: \n");
+    scanf("%f", &radius);
+    printf("area = %.2f", AREA(radius));
+
+	return 0;
+}
