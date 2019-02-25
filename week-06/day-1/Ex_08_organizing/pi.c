b14fd929fe7a9d1b7796e17ba0b5a4e707720fc2
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-02-25 13:50:31 +0100

    defining a variable and two function outside the main.c

---
+++ week-06/day-1/Ex_08_organizing/pi.c
@@ -0,0 +1,15 @@
+//
+// Created by Lilla on 2019. 02. 25..
+//
+#include "pi.h"
+#define PI 3.14
+
+float areaMeasure(float radius)
+{
+  return radius * radius * PI;
+}
+float circumferenceMeasure(float radius)
+{
+  return 2 * radius * PI;
+}
+
---
+++ week-06/day-1/Ex_08_organizing/pi.h
@@ -0,0 +1,11 @@
+//
+// Created by Lilla on 2019. 02. 18..
+//
+
+#ifndef EX_08_ORGANIZING_PI_H
+#define EX_08_ORGANIZING_PI_H
+
+float areaMeasure(float radius);
+float circumferenceMeasure(float radius);
+
+#endif //EX_08_ORGANIZING_PI_H
