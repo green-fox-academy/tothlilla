5837c5c8ce75a0d066239e1395674857cee6e5c5
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-03-14 20:20:51 +0100

    STM32F7 - note about basic BSP functions

---
+++ week-08/function_templates_SKELETON/important_BSP_functions.c
@@ -0,0 +1,19 @@
+#include "interruption_templates.h"
+//-------------------------------------
+//init PI11 button wit BSP
+//BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+//-------------------------------------
+//Returns the selected button state.
+//The Button GPIO pin value
+//BSP_PB_GetState(BUTTON_KEY);
+//-------------------------------------
+//init PI1 led  - LED1 or LED_GREEN
+//BSP_LED_Init(LED1);
+//-------------------------------------
+//turning on and off led
+//BSP_LED_On(LED1);
+//BSP_LED_Off(LED1);
+//BSP_LED_Toggle(LED1);
+
+
+
