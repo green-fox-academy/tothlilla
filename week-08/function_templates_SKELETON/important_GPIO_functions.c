bfebbd5496d9ac1797fce6469c2e3f54ca86f144
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-03-15 21:19:15 +0100

    STM32F7 - basic GPIO functions

---
+++ week-08/function_templates_SKELETON/important_GPIO_functions.c
@@ -0,0 +1,8 @@
+#include "interruption_templates.h"
+
+//turn on a led at F port 9 pin
+//HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET);
+//turn off a led at F port 9 pin
+//HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET);
+//Reads the specified input port pin, returns the input port pin value.
+//HAL_GPIO_ReadPin(GPIOB, external_button.Pin); //B port 4 pin - retval : 0 or 1
