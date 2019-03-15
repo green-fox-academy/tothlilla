e1d851973efe447f52a3615cb7a66acaa406c14b
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-03-15 19:09:45 +0100

    STM32F7 - commit usart init functions

--- week-08/function_templates_SKELETON/uart_init.c
+++ week-08/function_templates_SKELETON/uart_init.c
@@ -3,6 +3,7 @@
 void init_GPIO_BSP_uart()
 {
 //init uart for GPIO purpose with BSP
+	/* enable GPIO clock for A and B port*/
 	__HAL_RCC_USART1_CLK_ENABLE();
 
 	/* defining the UART configuration structure */
@@ -19,18 +20,19 @@ void init_GPIO_BSP_uart()
 
 void init_GPIO_uart()
 {
-//init uart for GPIO purpose without BSP
-	/* enable GPIO clock */
+//init uart for GPIO purpose with HAL
+	//PA9 port 9 pin with AF7 alternate function means USART1_TX - no visible/connecting pin
+	//PB9 port 7 pint with AF7 alternate function means USART1_RX - no visible/connecting pin
+	/* enable GPIO clock for A and B port*/
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 
 	/* configure GPIO for UART transmit line */
-	GPIOTxConfig.Pin = GPIO_PIN_9;
+	GPIOTxConfig.Pin = GPIO_PIN_9; //chose PA port 9 pin
 	GPIOTxConfig.Mode = GPIO_MODE_AF_PP;
 	GPIOTxConfig.Pull = GPIO_NOPULL;
 	GPIOTxConfig.Speed = GPIO_SPEED_FAST;
 	GPIOTxConfig.Alternate = GPIO_AF7_USART1;
-
 	HAL_GPIO_Init(GPIOA, &GPIOTxConfig);
 
 	/* configure GPIO for UART receive line */
@@ -39,7 +41,6 @@ void init_GPIO_uart()
 	GPIOTxConfig.Pull = GPIO_NOPULL;
 	GPIORxConfig.Speed = GPIO_SPEED_FAST;
 	GPIORxConfig.Alternate = GPIO_AF7_USART1;
-
 	HAL_GPIO_Init(GPIOB, &GPIORxConfig);
 
 	/* enable the clock of the used peripherial instance */
@@ -56,7 +57,6 @@ void init_GPIO_uart()
 
 	HAL_UART_Init(&uart);
 }
-
 
 //init uart for interrupt handle with BSP
 void init_uart(void)
