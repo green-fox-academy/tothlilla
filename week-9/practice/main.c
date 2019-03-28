f13664c987b3d884f197e1fd27213161202d4c8c
tothlilla <45636466+tothlilla@users.noreply.github.com>
2019-03-28 12:43:46 +0100

    init uart and test it

--- week-9/practice/main.c
+++ week-9/practice/main.c
@@ -9,12 +9,15 @@
 */
 #include "stm32f7xx.h"
 #include "stm32746g_discovery.h"
-			
+#define UART_PUTCHAR int __io_putchar(int ch)
+
 GPIO_InitTypeDef PA15_LED_config; //PA 15 pin able to compare channel 1 with alternate function AF
 GPIO_InitTypeDef user_button_handle; //PI 11 pin
 
 TIM_HandleTypeDef pwm_led_init;
 TIM_OC_InitTypeDef sConfig;
+
+UART_HandleTypeDef uart; //the uart config for general purpose
 
 void external_led_PA15_init(){
     __HAL_RCC_GPIOA_CLK_ENABLE(); //A port clock
@@ -25,6 +28,7 @@ void external_led_PA15_init(){
     PA15_LED_config.Alternate = GPIO_AF1_TIM2;	/* this alternate function we need to use TIM2 in output compare mode */
     HAL_GPIO_Init(GPIOA, &PA15_LED_config);
 }
+
 void pwm_init(){
 	//timer rész
 	pwm_led_init.Instance = TIM2;
@@ -58,11 +62,29 @@ void init_user_button(void)
 	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn); //enable the interrupt to HAL
 }
 
+void init_GPIO_BSP_uart()
+{
+	//init uart for GPIO purpose with BSP
+	__HAL_RCC_USART1_CLK_ENABLE();
+
+	uart.Instance = USART1;
+	uart.Init.BaudRate = 115200;
+	uart.Init.WordLength = UART_WORDLENGTH_8B;
+	uart.Init.StopBits = UART_STOPBITS_1;
+	uart.Init.Parity = UART_PARITY_NONE;
+	uart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	uart.Init.Mode = UART_MODE_TX_RX;
+
+	BSP_COM_Init(COM1, &uart);
+}
 
 int main(void)
 {
 	HAL_Init();
 	BSP_LED_Init(LED1);
+	init_user_button();
+	init_GPIO_BSP_uart();
+	printf("yes main\r\n");
 	while(1){
 
 	}
@@ -78,8 +100,15 @@ void EXTI15_10_IRQHandler(void)
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	if(GPIO_Pin == user_button_handle.Pin){
-		HAL_GPIO_TogglePin(LED1);//tesztelés miatt!!
+		BSP_LED_Toggle(LED1);//tesztelés miatt!!
+		printf("yes\r\n"); //tesztelés miatt
 	}
+}
+
+UART_PUTCHAR
+{
+    HAL_UART_Transmit(&uart, (uint8_t*)&ch, 1, 0xFFFF);
+    return ch;
 }
 
 
